5c7e56a62a582d731179f2a7757b76e8334ee570
Roman Vintonyak <19roman23@gmail.com>
2020-12-17 16:57:04 +0200

    create block florystyka- prezydia in galery page

--- templates/galery.php
+++ templates/galery.php
@@ -16,7 +16,7 @@
                         <div class="row wow rotateInUpLeft">
                             <?php foreach( $post_arka as $post ): 
                                 setup_postdata($post); ?>
-                                <div class="col-lg-4 col-md-12 mb-4">
+                                <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12 mb-4">
                                     <div class="view overlay zoom mask flex-center">
                                         <figure class="photo">
                                             <a href="<?php the_field('img_post'); ?>" data-lightbox="roadtrip" alt="wedding">
@@ -37,7 +37,7 @@
                             <div class="row wow rotateInUpLeft">
                                 <?php foreach( $post_fotozona as $post ): 
                                     setup_postdata($post); ?>
-                                    <div class="col-lg-4 col-md-12 mb-4">
+                                    <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12 mb-4">
                                         <div class="view overlay zoom mask flex-center">
                                             <figure class="photo">
                                                 <a href="<?php the_field('img_post'); ?>" data-lightbox="roadtrip" alt="wedding">
@@ -51,135 +51,51 @@
                             <?php 
                             wp_reset_postdata(); ?>
                         <?php endif; ?>
-
-
-                <!--<h4 class="mb-5 font-weight-bold h2">Зона молодих</h4>
-                <div class="row wow rotateInUpLeft">
-                    <div class="col-lg-4 col-md-12 mb-4">
-                        <div class="view overlay zoom mask flex-center">
-                            <figure class="photo">
-                                <a href="../img/galery/pz/big/1.jpeg" data-lightbox="roadtrip" alt="wedding"><img
-                                        id="pr1"
-                                        src="../img/galery/pz/small/1.jpeg"
-                                        alt="wedding"/></a>
-                            </figure>
-                        </div>
-                    </div>
-                    <div class="col-lg-4 col-md-6 mb-4">
-                        <div class="view overlay zoom mask flex-center">
-                            <figure class="photo">
-                                <a href="../img/galery/pz/big/2.jpeg" data-lightbox="roadtrip" alt="wedding"><img
-                                        id="pr2"
-                                        src="../img/galery/pz/small/2.jpeg"
-                                        alt="wedding"/></a>
-                            </figure>
-                        </div>
-                    </div>
-                    <div class="col-lg-4 col-md-6 mb-4">
-                        <div class="view overlay zoom mask flex-center">
-                            <figure class="photo">
-                                <a href="../img/galery/pz/big/5.jpeg" data-lightbox="roadtrip" alt="wedding"><img
-                                        id="pr5"
-                                        src="../img/galery/pz/small/5.jpeg"
-                                        alt="wedding"/></a>
-                            </figure>
-                        </div>
-                    </div>
-                    <div class="col-lg-4 col-md-12 mb-4">
-                        <div class="view overlay zoom mask flex-center">
-                            <figure class="photo">
-                                <a href="../img/galery/pz/big/4.jpeg" data-lightbox="roadtrip" alt="wedding"><img
-                                        id="pr4"
-                                        src="../img/galery/pz/small/4.jpeg"
-                                        alt="wedding"/></a>
-                            </figure>
-                        </div>
-                    </div>
-                    <div class="col-lg-4 col-md-6 mb-4">
-                        <div class="view overlay mask flex-center zoom">
-                            <figure class="photo">
-                                <a href="../img/galery/pz/big/6.jpeg" data-lightbox="roadtrip" alt="wedding"><img
-                                        id="pr6"
-                                        src="../img/galery/pz/small/6.jpeg"
-                                        alt="wedding"/></a>
-                            </figure>
-                        </div>
-                    </div>
-                    <div class="col-lg-4 col-md-6 mb-4">
-                        <div class="view overlay mask flex-center zoom">
-                            <figure class="photo">
-                                <a href="../img/galery/pz/big/3.jpg" data-lightbox="roadtrip" alt="wedding"><img
-                                        id="pr3"
-                                        src="../img/galery/pz/small/3.jpg"
-                                        alt="wedding"/></a>
-                            </figure>
-                        </div>
-                    </div>
-                </div>
-                <h4 class="mb-5 font-weight-bold h2">Флористика</h4>
-                <div class="row wow rotateInUpRight">
-                    <div class="col-lg-4 col-md-12 mb-4">
-                        <div class="view overlay zoom mask flex-center">
-                            <figure class="photo">
-                                <a href="../img/galery/kvity/big/1.jpg" data-lightbox="roadtrip" alt="wedding"><img
-                                        id="kv1"
-                                        src="../img/galery/kvity/small/1.jpeg"
-                                        alt="wedding"/></a>
-                            </figure>
-                        </div>
-                    </div>
-                    <div class="col-lg-4 col-md-6 mb-4">
-                        <div class="view overlay zoom mask flex-center">
-                            <figure class="photo">
-                                <a href="../img/galery/kvity/big/2.jpeg" data-lightbox="roadtrip" alt="wedding"><img
-                                        id="kv2"
-                                        src="../img/galery/kvity/small/2.jpeg"
-                                        alt="wedding"/></a>
-                            </figure>
-                        </div>
-                    </div>
-                    <div class="col-lg-4 col-md-6 mb-4">
-                        <div class="view overlay zoom mask flex-center">
-                            <figure class="photo">
-                                <a href="../img/galery/kvity/big/3.jpeg" data-lightbox="roadtrip" alt="wedding"><img
-                                        id="kv3"
-                                        src="../img/galery/kvity/small/3.jpeg"
-                                        alt="wedding"/></a>
-                            </figure>
-                        </div>
-                    </div>
-                    <div class="col-lg-4 col-md-12 mb-4">
-                        <div class="view overlay zoom mask flex-center">
-                            <figure class="photo">
-                                <a href="../img/galery/kvity/big/4.jpeg" data-lightbox="roadtrip" alt="wedding"><img
-                                        id="kv4"
-                                        src="../img/galery/kvity/small/4.jpeg"
-                                        alt="wedding"/></a>
-                            </figure>
-                        </div>
-                    </div>
-                    <div class="col-lg-4 col-md-6 mb-4">
-                        <div class="view overlay mask flex-center zoom">
-                            <figure class="photo">
-                                <a href="../img/galery/kvity/big/5.jpeg" data-lightbox="roadtrip" alt="wedding"><img
-                                        id="kv5"
-                                        src="../img/galery/kvity/small/5.jpeg"
-                                        alt="wedding"/></a>
-                            </figure>
-                        </div>
-                    </div>
-                    <div class="col-lg-4 col-md-6 mb-4">
-                        <div class="view overlay mask flex-center zoom">
-                            <figure class="photo">
-                                <a href="../img/galery/kvity/big/6.jpeg" data-lightbox="roadtrip" alt="wedding"><img
-                                        id="kv6"
-                                        src="../img/galery/kvity/small/6.jpeg"
-                                        alt="wedding"/></a>
-                            </figure>
-                        </div>
-                    </div>
-                </div>
-                <h4 class="mb-5 font-weight-bold h2">Поліграфія</h4>
+                <h4 class="mb-5 font-weight-bold h2"><?php the_field('prezydiya_title'); ?></h4>
+                    <?php
+                        $post_prezydia = get_field('galery_prezydia');
+                        if( $post_prezydia ): ?>
+                            <div class="row wow rotateInUpLeft">
+                                <?php foreach( $post_prezydia as $post ): 
+                                    setup_postdata($post); ?>
+                                    <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12 mb-4">
+                                        <div class="view overlay zoom mask flex-center">
+                                            <figure class="photo">
+                                                <a href="<?php the_field('img_post'); ?>" data-lightbox="roadtrip" alt="wedding">
+                                                    <img src="<?php the_field('miniatyura_galereyi'); ?>" alt="wedding"/>
+                                                </a>
+                                            </figure> 
+                                        </div>
+                                    </div>
+                                <?php endforeach; ?>
+                            </div>
+                            <?php 
+                            wp_reset_postdata(); ?>
+                    <?php endif; ?>
+                <h4 class="mb-5 font-weight-bold h2"><?php the_field('florystyka_title'); ?></h4>
+                    <?php
+                        $post_florystyka = get_field('galery_florystyka');
+                        if( $post_florystyka ): ?>
+                            <div class="row wow rotateInUpLeft">
+                                <?php foreach( $post_florystyka as $post ): 
+                                    setup_postdata($post); ?>
+                                    <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12 mb-4">
+                                        <div class="view overlay zoom mask flex-center">
+                                            <figure class="photo">
+                                                <a href="<?php the_field('img_post'); ?>" data-lightbox="roadtrip" alt="wedding">
+                                                    <img src="<?php the_field('miniatyura_galereyi'); ?>" alt="wedding"/>
+                                                </a>
+                                            </figure> 
+                                        </div>
+                                    </div>
+                                <?php endforeach; ?>
+                            </div>
+                            <?php 
+                            wp_reset_postdata(); ?>
+                    <?php endif; ?>
+                
+                
+                <!--<h4 class="mb-5 font-weight-bold h2">Поліграфія</h4>
                 <div class="row wow rotateInUpLeft">
                     <div class="col-lg-4 col-md-12 mb-4">
                         <div class="view overlay zoom mask flex-center">
